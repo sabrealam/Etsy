7b61dfb2c74380695652d3bedaaca9cb246decd8
Sabre Alam <142332803+sabrealam@users.noreply.github.com>
2024-02-29 09:33:22 +0545

    Checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react' 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() { 
 
   return (
     <>
       <h1>Counter</h1>
+      <Navbar/>
     </>
   )
 }
